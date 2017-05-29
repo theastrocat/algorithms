42ed78b922b60593f5de7a042ce05a45964a5c49
theastrocat <brian.mcadams88@gmail.com>
2017-05-29 12:03:30 -0700

    Add depth first search algorithm.

--- src/search.py
+++ src/search.py
@@ -6,6 +6,9 @@ Common search algorithms.
 
 def breadthfirst(graph,start, end):
     """
+    Breadth First Search Algorithm.
+    Returns shortest path and path taken as tuple.
+
     Inputs:
         graph : Dictionary of connecting nodes
         start : Integer starting point on the graph
@@ -27,3 +30,49 @@ def breadthfirst(graph,start, end):
             new_path = list(path)
             new_path.append(adjacent)
             queue.append(new_path)
+
+def depthfirst(graph, start, end):
+    """
+    Depth First Search algorithm,
+    returns path takend so far...
+    -----------------------------
+    Inputs:
+        graph : Dictionary of connecting nodes
+        start : Integer starting point on the graph
+        end   : Integer ending point on the graph
+    Outputs:
+        tpath  : Path taken
+    """
+    queue = []
+    queue.append(start)
+    tpath = []
+    while queue:
+        node = queue.pop(0)
+        tpath.append(node)
+        newnodes = graph.get(node, [])
+        if node == end:
+            return tpath
+        queue = newnodes + queue
+
+def depthfirstright(graph, start, end):
+    """
+    Depth First Search algorithm,
+    returns path takend so far...
+    -----------------------------
+    Inputs:
+        graph : Dictionary of connecting nodes
+        start : Integer starting point on the graph
+        end   : Integer ending point on the graph
+    Outputs:
+        tpath  : Path taken
+    """
+    queue = []
+    queue.append(start)
+    tpath = []
+    while queue:
+        node = queue.pop(-1)
+        tpath.append(node)
+        newnodes = graph.get(node, [])
+        if node == end:
+            return tpath
+        queue.extend(newnodes)
